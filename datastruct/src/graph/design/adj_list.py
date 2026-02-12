7ad82a06e2d77af3d27aef6573bf3126cd6714bf
jeet.gupta <jeetgupta@gmail.com>
2026-02-12 13:08:53 -0500

    adj list implementation

---
+++ datastruct/src/graph/design/adj_list.py
@@ -0,0 +1,27 @@
+"""
+Two Implementation Approaches:
+1) Using graph nodes to represent each vertice in an adjancency list
+2) Just using a hashmap to map each key (vertice) to its neighbors
+"""
+#graph node used for adj list
+class GraphNode:
+    def __init__(self, val):
+        self.val = val
+        self.neighbours = []
+
+#or just use a hashmap
+adj_list = {"A": [], "B": []}
+
+# Given directed edges, build an adjacency list
+edges = [["A", "B"], ["B", "C"], ["B", "E"], ["C", "E"], ["E", "D"]]
+
+def create_graph(adj_list, edge_list):
+    for src, dst in edge_list:
+        if src not in adj_list:
+            adj_list[src] = []
+        if dst not in adj_list:
+            adj_list[dst] = []
+        adj_list[src].append(dst)
+
+create_graph(adj_list, edges)
+print(adj_list)
